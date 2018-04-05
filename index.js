347d6434d3598387653c8103e2af8ab273cef746
guyiluz <iloz.guy@gmail.com>
2018-04-05 10:27:40 +0300

    added core

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const  port= process.env.PORT || 3000
 
 
 const data = require('./groups.json')
-
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
 app.get('/groups', (req, res)=>{
 res.send(data)
 
