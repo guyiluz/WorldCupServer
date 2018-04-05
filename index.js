e1494bf8e316bfc005c07453d9713f87e2d0db4a
guyiluz <iloz.guy@gmail.com>
2018-04-05 10:30:57 +0300

    added core with npm

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const  port= process.env.PORT || 3000
+const cors = require('cors')
+
 
 
 
 const data = require('./groups.json')
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-   });
+app.use(cors())
+
 app.get('/groups', (req, res)=>{
 res.send(data)
 
